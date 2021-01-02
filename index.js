1e6c118702ec53632bae1710ea529e2dba323796
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-01-02 11:42:43 +0900

    gitpubのapiを叩く

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log('index.js: loaded');
+
+// CSSセレクタを使ってDOMツリー中のh2要素を取得する
+const heading = document.querySelector("h2");
+// h2要素に含まれるテキストコンテンツを取得する
+const headingText = heading.textContent;
+
+// button要素を作成する
+const button = document.createElement("button");
+button.textContent = "Push Me";
+// body要素の子要素としてbuttonを挿入する
+document.body.appendChild(button);
+
+// githubのapiを叩く
+const userId = 'yuki5664';
+function fetchUserInfo(userId) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+      console.log(response.status);
+      if (!response.ok) {
+        console.error('エラーレスポンス', response);
+      } else {
+        return response.json().then(userInfo => {
+          console.log(userInfo);
+        });
+      }
+    }).catch(error => {
+      console.error(error);
+    });
+};
