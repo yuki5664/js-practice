8069e6468b8bc3908f90820c80e6e25e694184cf
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-01-03 12:33:21 +0900

    Add refactoring

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
-console.log('index.js: loaded');
-
-// CSSセレクタを使ってDOMツリー中のh2要素を取得する
-const heading = document.querySelector("h2");
-// h2要素に含まれるテキストコンテンツを取得する
-const headingText = heading.textContent;
-
-// button要素を作成する
-const button = document.createElement("button");
-button.textContent = "Push Me";
-// body要素の子要素としてbuttonを挿入する
-document.body.appendChild(button);
-
-// githubのapiを叩く
-const userInfo = 'js-primer-example';
-function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then(response => {
-      console.log(response.status);
-      if (!response.ok) {
-        console.error('エラーレスポンス', response);
-      } else {
-        return response.json().then(userInfo => {
-          // アバターの情報用
-          const view = `
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-              <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
-          // HTMLの挿入
-          const result = document.getElementById("result");
-          result.innerHTML = view;
-          console.log(userInfo);
+function main() {
+    fetchUserInfo("yuki5664")
+        // ここではJSONオブジェクトで解決されるPromise
+        .then((userInfo) => createView(userInfo))
+        // ここではHTML文字列で解決されるPromise
+        .then((view) => displayView(view))
+        // Promiseチェーンでエラーがあった場合はキャッチされる
+        .catch((error) => {
+            console.error(`エラーが発生しました (${error})`);
         });
-      }
-    }).catch(error => {
-      console.error(error);
-    });
-};
+}
 
+function fetchUserInfo(userId) {
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+            } else {
+                // JSONオブジェクトで解決されるPromiseを返す
+                return response.json();
+            }
+        });
+}
+
+function createView(userInfo) {
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+    const result = document.getElementById("result");
+    result.innerHTML = view;
+}
+
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
+}
